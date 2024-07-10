40a05ff3ec6aba242444451d3a43b648f76eba16
adeard <adeardian1994@gmail.com>
2024-07-10 16:15:55 +0700

    update swagger

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 )
 
-// @Host 202.158.14.235:4222
+// @Host https://service.indoagri.co.id/InspectionRA
 // @title API SWAGGER FOR INSPECTION RA API SERVICE
 // @version 1.0.0
 // @description INSPECTION RA API SERVICE
